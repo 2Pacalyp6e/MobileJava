0c2395cf03e0a22dac641ba3ccb9e306751b25fb
2Pac <pachacuti555@mail.ru>
2019-05-06 17:56:09 +0600

    Домашнее задание по 2 уроку

---
+++ MainClass.java
@@ -0,0 +1,53 @@
+public class MainClass {
+    public static void main(String[] args) {
+
+///////////////////Первое задание
+//        int [] arrz = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0 };
+//        for (int i = 0; i < arrz.length; i++) {
+//            arrz[i] = arrz[i] == 1 ? 0:1;
+//            System.out.print(arrz[i] + " ");
+//        }
+
+/////////////////////второе задание
+
+//        int [] arrt = new int[8];
+//        for (int i = 1, j = 0; i < arrt.length; i++) arrt[i] = j+= 3;
+//        for (int y: arrt) System.out.print(y + " ");
+
+///////////////////третье задание        
+//int [] arrs = {1,5,3,2,11,4,5,2,4,8,9,1};
+//            for (int i = 0; i < arrs.length ; i++) {
+//
+//                if (arrs[i] < 6) arrs[i] *=2;
+//                System.out.print(arrs[i] + " ");
+
+/////////////////////четвертое задание
+//        int[][] arrdi = new int[5][5];
+//        for (int i = 0; i < arrdi.length; i++) {
+//            for (int j = 0, j2 = arrdi[i].length; j < arrdi[i].length; j++, j2--) {
+//                if (i == j || i == (j2 - 1)) arrdi[i][j] = 1;
+//                System.out.print(arrdi[i][j] + " ");
+//            }
+//            System.out.print("\r\n");
+
+////////////////////////пятое задание
+
+//        int [] arrminmax = {12,4124,221,2,54,77,1535};
+//        int min=arrminmax[0], max =arrminmax[0], maxi=0, mini=0;
+//        for (int i = 0; i < arrminmax.length ; i++) {
+//            if (arrminmax[i] > max) {
+//                max = arrminmax[i];
+//                maxi = i+1;
+//            }
+//            if (arrminmax[i] < min) {
+//                min = arrminmax[i];
+//                mini = i+1;
+//            }
+//        }
+//            System.out.println("Макскимальное число в ячейке " + maxi  +  " Число равное " + max);
+//            System.out.println("Минимальное число в ячейке " + mini  + " Число равное " + min);
+
+
+        /////////////////////////////////////Шестое задание
+    }
+}
