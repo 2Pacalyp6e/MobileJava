b64874615fa67b6096c3f21e3071ae5283021f2f
2pacalyp6e <pachacuti555@mail.ru>
2019-05-13 19:25:52 +0900

    Домашнее Задание по 3 Уроку-готово

--- MainClass.java
+++ MainClass.java
@@ -1,53 +1,109 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class MainClass {
+    Scanner scanner = new Scanner(System.in);
+    static Random random = new Random();
+
     public static void main(String[] args) {
 
-///////////////////Первое задание
-//        int [] arrz = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0 };
-//        for (int i = 0; i < arrz.length; i++) {
-//            arrz[i] = arrz[i] == 1 ? 0:1;
-//            System.out.print(arrz[i] + " ");
-//        }
 
-/////////////////////второе задание
-
-//        int [] arrt = new int[8];
-//        for (int i = 1, j = 0; i < arrt.length; i++) arrt[i] = j+= 3;
-//        for (int y: arrt) System.out.print(y + " ");
-
-///////////////////третье задание        
-//int [] arrs = {1,5,3,2,11,4,5,2,4,8,9,1};
-//            for (int i = 0; i < arrs.length ; i++) {
-//
-//                if (arrs[i] < 6) arrs[i] *=2;
-//                System.out.print(arrs[i] + " ");
-
-/////////////////////четвертое задание
-//        int[][] arrdi = new int[5][5];
-//        for (int i = 0; i < arrdi.length; i++) {
-//            for (int j = 0, j2 = arrdi[i].length; j < arrdi[i].length; j++, j2--) {
-//                if (i == j || i == (j2 - 1)) arrdi[i][j] = 1;
-//                System.out.print(arrdi[i][j] + " ");
-//            }
-//            System.out.print("\r\n");
-
-////////////////////////пятое задание
-
-//        int [] arrminmax = {12,4124,221,2,54,77,1535};
-//        int min=arrminmax[0], max =arrminmax[0], maxi=0, mini=0;
-//        for (int i = 0; i < arrminmax.length ; i++) {
-//            if (arrminmax[i] > max) {
-//                max = arrminmax[i];
-//                maxi = i+1;
-//            }
-//            if (arrminmax[i] < min) {
-//                min = arrminmax[i];
-//                mini = i+1;
-//            }
-//        }
-//            System.out.println("Макскимальное число в ячейке " + maxi  +  " Число равное " + max);
-//            System.out.println("Минимальное число в ячейке " + mini  + " Число равное " + min);
+        Scanner scanner = new Scanner(System.in);
 
 
-        /////////////////////////////////////Шестое задание
+//                    GuessNumber(scanner);
+                    GuessFruit();
     }
+
+
+    public static void GuessNumber(Scanner scanner){
+        do {
+            int numb = new Random().nextInt(10);
+            int i;
+            System.out.println("Угадайте число от 0 до 9 с трёх попыток, Желаем Удачи!");
+            for ( i = 0; i < 3; i++) {
+                System.out.printf("Введите ваше число #%d: ", i);
+                int Vvod=scanner.nextInt();
+                if (numb > Vvod) {
+                    System.out.println("Не верно, Ваше число Меньше!");
+                } else if (numb<Vvod){
+                    System.out.println("Мимо,Ваше число Больше");
+                }else {
+                    System.out.println(" Вы Угадали! ");
+                    break;
+                }
+
+            }
+            if(i == 3) {
+                System.out.println("Вы проиграли, ваши попытки закончились...(");
+            }
+            System.out.println("Хотите сыграть еще раз? 1 - ДА, хочу, 0 - Нет, мне и так весело)");
+        } while (scanner.nextInt()==1);
+    }
+
+
+    ////////////////////////////
+
+    private static void GuessFruit() {
+
+        String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot", "cherry",
+                "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea", "peanut",
+                "pear", "pepper", "pineapple", "pumpkin", "potato"};
+
+        int ZWord = random.nextInt(words.length - 1);
+        String word = words[ZWord];
+        int DlWord = word.length();
+        System.out.println("Мы загадали слово,  отгадайте его");
+        System.out.println(word);
+        Scanner scanner = new Scanner(System.in);
+        do {
+
+            System.out.println("Ващ ответ: (для выхода из игры, нажмите Enter)");
+            String answer = scanner.nextLine();
+
+            if (answer.equals("")) break;
+            else if (word.equals(answer)) {
+                System.out.println("Вы угадали!!!");
+                break;
+            }
+
+            char[] charsAnswer = answer.toCharArray();
+            for (int i = 0; i < DlWord; i++) {
+                if (i >= charsAnswer.length) break;
+                if (word.charAt(i) != charsAnswer[i]) charsAnswer[i] = '*';
+            }
+            StringBuilder comment = new StringBuilder(String.valueOf(charsAnswer));
+            for (int i = comment.length(); i < 15; i++) comment.append("*");
+            System.out.println(comment);
+        }
+        while (true);
+    }
+
+
+
+
+
+
+
+    //////////////////////////////////////////////Второе задание
+//        public static String GuessWord(String a1, String a2) {
+//
+//         int minDl = Math.min(a1.length(), a2.length ());
+//            for (int i = 0; i < minDl ; i++) {
+//            if (a1.charAt(i) == a2.charAt(i)){
+//                System.out.print(a1.charAt(i));
+//            } else{
+//                System.out.print("*");
+//            }
+//
+//        }
+//            for (int i = minDl; i < 15; i++) {
+//                System.out.print("*");
+//
+//            }
+//            return a1;
+//    }
+
 }
+
+
